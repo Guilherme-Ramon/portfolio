c216aad8b50d825fe40005f76134ee325f16030e
Guilherme-Ramon <guilhermeramon996@gmail.com>
2024-12-31 10:31:07 -0300

    Reinicializando o repositório após a remoção da pasta .git

---
+++ src/enviar-email.js
@@ -0,0 +1,30 @@
+// Inicializa o EmailJS com a chave pública
+(function () {
+    emailjs.init("3qeW_wLmuQXVhICss"); // Substitua pela sua chave pública
+})();
+
+const btn = document.getElementById("btn-entrar-contato"); // O botão para envio
+
+// Adicionando o evento de envio do formulário
+
+document.getElementById("form").addEventListener("submit", function (event) {
+    event.preventDefault(); // Previne o comportamento padrão do formulário
+
+    btn.innerText = "Enviando..."; // Altera o texto do botão para indicar que está enviando
+
+    const serviceID = "service_gto6qzz"; // Substitua pelo seu service ID
+    const templateID = "template_jdhjzga"; // Substitua pelo seu template ID
+
+    // Envia o formulário usando o método sendForm do EmailJS
+    emailjs.sendForm(serviceID, templateID, this).then(
+        function () {
+            btn.innerText = "Mensagem Enviada"; // Altera o texto para indicar sucesso
+            alert("Mensagem enviada com sucesso!");
+            document.getElementById("form").reset(); // Limpa o formulário
+        },
+        function (err) {
+            btn.innerText = "Enviar Mensagem"; // Restaura o texto do botão em caso de erro
+            alert("Erro ao enviar a mensagem: " + JSON.stringify(err));
+        }
+    );
+});
---
+++ src/script.js
@@ -0,0 +1,73 @@
+document.addEventListener("DOMContentLoaded", function () {
+  async function carregarProjetos() {
+    try {
+      const response = await fetch('projetos.json');
+      const projetos = await response.json();
+      const container = document.getElementById('projetos-container');
+
+      projetos.forEach(projeto => {
+        container.innerHTML += `
+          <div class="col-md-6 col-lg-4 my-3">
+            <div class="card bg-dark-custom text-light shadow-sm border-0 rounded-lg overflow-hidden">
+              <img src="${projeto.imagem}" class="card-img-top projeto-img" alt="${projeto.titulo}" />
+              <div class="card-body p-4">
+                <h5 class="card-title text-light font-weight-bold">${projeto.titulo}</h5>
+                <p class="card-text text-light">${projeto.descricao}</p>
+                <a href="${projeto.link}" class="btn btn-ver-projeto w-100 fs-5 fw-bold" target="_blank">Ver Projeto</a>
+              </div>
+            </div>
+          </div>
+        `;
+      });
+
+      document.querySelectorAll('#projetos-container a').forEach(link => {
+        link.addEventListener('click', (e) => {
+          if (!link.getAttribute('href') || link.getAttribute('href') === '#') {
+            e.preventDefault();
+            Swal.fire({
+              icon: 'info',
+              title: 'Indisponível',
+              text: 'Projeto disponível em breve!',
+              confirmButtonText: 'Entendido'
+            });
+          }
+        });
+      });
+    } catch (error) {
+      console.error("Erro ao carregar os projetos:", error);
+    }
+  }
+
+  async function carregarHabilidades() {
+    try {
+      const response = await fetch('habilidades.json');
+      const habilidades = await response.json();
+      const container = document.getElementById('habilidades-container');
+
+      habilidades.forEach(habilidade => {
+        container.innerHTML += `
+          <div class="col-6 col-md-4">
+            <i class="bi ${habilidade.icone} ${habilidade.cor} fs-1" style="font-size: calc(5vw + 10px)"></i>
+            <h4 class="mt-2">${habilidade.nome}</h4>
+          </div>
+        `;
+      });
+    } catch (error) {
+      console.error("Erro ao carregar as habilidades:", error);
+    }
+  }
+
+  carregarProjetos();
+  carregarHabilidades();
+
+  document.addEventListener('click', function(event) {
+    var navbar = document.getElementById('navbarNav');
+    if (!navbar.contains(event.target) && !event.target.closest('.navbar-toggler') && navbar.classList.contains('show')) {
+      new bootstrap.Collapse(navbar).toggle();
+    }
+  });
+
+  document.getElementById('navbarNav').addEventListener('mouseleave', function() {
+    if (this.classList.contains('show')) new bootstrap.Collapse(this).toggle();
+  });
+});
